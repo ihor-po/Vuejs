694b7ae0e26d17ab7e87a18d24c30f41ebc1b93a
Ihor Porotikov <>
2022-05-07 12:18:04 +0300

    add basic reactivity

--- basics_events/app.js
+++ basics_events/app.js
@@ -5,8 +5,40 @@ const app = Vue.createApp({
       addNumber: 10,
       reduceNumber: 5,
       name: '',
-      confirmedName: ''
+      lastname: '',
+      confirmedName: '',
+      // fullname: ''
     };
+  },
+  watch: {
+    counter(value) {
+      const that = this;
+      if (value > 50) {
+        setTimeout(function () {
+          that.counter =  0;
+        }, 2000);
+      }
+      
+    }
+    // name(value, oldValue) {
+    //   this.fullname = value !== '' 
+    //   ? value + ' ' + this.lastname
+    //   : ''; 
+    // },
+    // lastname(value, oldValue) {
+    //   this.fullname = value !== '' 
+    //   ? this.name + ' ' + value
+    //   : '';
+    // }
+  },
+  computed: {
+    fullname() {
+      if (this.name === '' && this.lastname === '') {
+        return '';
+      }
+
+      return this.name + ' ' + this.lastname;
+    }
   },
   methods: {
     add(number) {
@@ -16,13 +48,19 @@ const app = Vue.createApp({
       this.counter -= number;
     },
     setName(event) {
+      console.log(this.name);
       this.name = event.target.value;
     },
     confirmName() {
-      this.confirmedName = this.name;
+      this.confirmedName = this.name + ' ' + this.lastname;
     },
     submitForm() {
       alert('It\'s works');
+    },
+    resetInputs() {
+      // document.querySelector('input').value = "";
+      this.name = '';
+      this.lastname = '';
     }
   }
 });
